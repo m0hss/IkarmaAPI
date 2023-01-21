ee56b5024798a654ff3613b1dfba8fd5c1a0d5ad
m0 <M0@mohamedfix-it.com>
2023-01-22 00:49:52 +0100

    heroku deploy

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-USER_DATABASE_URL = "mysql+mysqlconnector://root@localhost:3306/ikarma"
+USER_DATABASE_URL = "mysql+mysqlconnector://b7890773c1228a:181896d6@eu-cdbr-west-03.cleardb.net/heroku_fb822d9648d206b"
+# USER_DATABASE_URL = "mysql+mysqlconnector://root@localhost:3306/ikarma"
 
 engine = create_engine(
     USER_DATABASE_URL
