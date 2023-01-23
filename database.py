0835c64356980e352984cb0d14fcac5cf2f819a6
m0 <M0@mohamedfix-it.com>
2023-01-23 03:08:11 +0100

    mysql connector

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-USER_DATABASE_URL = "mysql+mysqlconnector://root@localhost:3306/ikarma"
+USER_DATABASE_URL = "mysql+mysqlconnector://root:qzDjhBdNJdVa0IHcSdJ9@containers-us-west-38.railway.app:6121/railway"
 
 engine = create_engine(
     USER_DATABASE_URL
