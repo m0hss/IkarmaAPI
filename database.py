d18f5cd3e792abe5ac92efce85b7a47a8936b0be
m0 <M0@mohamedfix-it.com>
2023-01-24 01:17:17 +0100

    .env

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-USER_DATABASE_URL = "mysql+mysqlconnector://root@localhost:3306/ikarma"
+# USER_DATABASE_URL = "postgresql://m0:vMqDPtG4avjyxRzX7vOpIlfan8QqMfBF@dpg-cf7ch9mn6mplrj35m8u0-a.frankfurt-postgres.render.com/ikarma_api_pgsql"
 
 engine = create_engine(
     USER_DATABASE_URL
